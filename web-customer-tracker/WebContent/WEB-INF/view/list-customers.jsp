505e0940734e74e38dde8a727e71104e08b3d77c
Jaceimet <84209692+Jaceimet@users.noreply.github.com>
2023-01-23 18:19:47 -0800

    Add customer functionality
    adding a add customer button to the app. Modifying app to accomidate.
    test run with simple add customer page. Success.

--- web-customer-tracker/WebContent/WEB-INF/view/list-customers.jsp
+++ web-customer-tracker/WebContent/WEB-INF/view/list-customers.jsp
@@ -29,6 +29,14 @@
 	
 		<div id="content">
 		
+			<!-- put new button: Add Customer -->
+			
+			<input type="button" value="Add Customer"
+					onclick="window.location.href='showFormForAdd'; return false;"
+					class="add-button"
+			
+			/>
+		
 			<!-- add out html table here -->
 		
 			<table>
--- web-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
@@ -5,24 +5,27 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.luv2code.springdemo.dao.CustomerDAO;
 import com.luv2code.springdemo.entity.Customer;
+import com.luv2code.springdemo.service.CustomerService;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 	
-	// need to inject DAO into controller
+	// need to inject customer service
 	@Autowired
-	private CustomerDAO customerDAO;
+	private CustomerService customerService;
 	
 	@RequestMapping("/list")
 	public String listCustomers(Model theModel) {
 		
-		// get customers from DAO
-		List<Customer> theCustomers = customerDAO.getCustomers();
+		// get customers from the service
+		List<Customer> theCustomers = customerService.getCustomers();
 		
 		// add the customers to the model
 		theModel.addAttribute("customers", theCustomers);
@@ -30,5 +33,28 @@ public class CustomerController {
 		return "list-customers";
 	}
 	
+	
+	@GetMapping("/showFormForAdd")
+	public String showForForAdd(Model theModel) {
+		
+		// create model attribute to bind form data
+		Customer theCustomer = new Customer();
+		
+		theModel.addAttribute("customer", theCustomer);
+		
+		
+		return "customer-form";
+		
+	}
+	
 
 }
+
+
+
+
+
+
+
+
+
