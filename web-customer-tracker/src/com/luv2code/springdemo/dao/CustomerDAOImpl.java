535cfeb82a99bafa1974ee4a3b614ebcf7c31c56
Jaceimet <84209692+Jaceimet@users.noreply.github.com>
2023-01-23 21:55:35 -0800

    Connect input data to DB
    Connect input data to DB by using saveOrUpdate through hibernate. Test
    proves successful update of Mary. Verified through DB.

--- web-customer-tracker/src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
+++ web-customer-tracker/src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -46,8 +46,8 @@ public class CustomerDAOImpl implements CustomerDAO {
 		//get current hibernate session
 		Session currentSession = sessionFactory.getCurrentSession();
 		
-		// save the customer . . . finaly
-		currentSession.save(theCustomer);
+		// save/update the customer . . . finaly
+		currentSession.saveOrUpdate(theCustomer);
 	}
 
 
